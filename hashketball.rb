44bd51c3575932721708c74178430e27f53097f1
Nora Vogt <noraevogt@gmail.com>
2018-11-27 15:37:59 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,27 +2,36 @@
 def game_hash
   hash = {
     :home => {
-                :team_name => "", 
-                :colors => [], 
-                :players => {
-                  
+                :team_name => "Brooklyn Nets",
+                :colors => ["Black", "White"],
+                :players => { "Alan Anderson" => {
+                                                    :number => "0",
+                                                    :shoe => "16",
+                                                    :points => "22",
+                                                    :rebounds => "12",
+                                                    :assists => "12",
+                                                    :steals => "3",
+                                                    :blocks => "1",
+                                                    :slam_dunks => "1",
+                                                  },
+
                 }
               },
     :away => {
-              :team_name => "", 
-              :colors => [], 
-              :players => {
+              :team_name => "",
+              :colors => [],
+              :players => { "name" => {
+                                        :number => "",
+                                        :shoe => "",
+                                        :points => "",
+                                        :rebounds => "",
+                                        :assists => "",
+                                        :steals => "",
+                                        :blocks => "",
+                                        :slam_dunks => "",
+                                      },
                 
               }
             }
   }
 end
-
-
-
-
-
-
-
-
-
